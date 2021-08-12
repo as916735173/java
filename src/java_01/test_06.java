9578cb1b64419b47a4f77881d7aab2bb12829aa9
chaz <81009972+as916735173@users.noreply.github.com>
2021-08-12 14:57:51 +0800

    Initial commit

--- src/java_01/test_06.java
+++ src/java_01/test_06.java
@@ -2,9 +2,13 @@ package java_01;
 
 public
 class test_06 {
-    public static void main(String[] arg){
-        long num=2147483649L;
-        int temp=(int) num;
-        System.out.println (temp);
+    public static void main(String[] arg) {
+        byte max = Byte.MAX_VALUE;
+        byte min = Byte.MIN_VALUE;
+        int x=200;
+        byte num=(byte) x;//int类型为常量会自动转类型，是变量的话需要强制转换
+        System.out.println (num);
+        System.out.println (max);
+        System.out.println (min);
     }
 }
---
+++ src/java_01/test_07.java
@@ -0,0 +1,10 @@
+package java_01;
+
+public
+class test_07 {
+    public static void main(String[] arg){
+        long num=2147483649L;
+        int temp=(int) num;
+        System.out.println (temp);
+    }
+}
