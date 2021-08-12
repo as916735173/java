dc57414467a72f9de2b093b9f5dc41feff617d60
chaz <81009972+as916735173@users.noreply.github.com>
2021-08-12 16:00:31 +0800

    Initial commit

---
+++ src/java_01/test_09.java
@@ -0,0 +1,15 @@
+package java_01;
+
+public
+class test_09 {
+    public static void main(String[] arg){
+       char ch = 'A';//一个字符编码
+       int num_01 = ch;
+       System.out.println (ch);
+       System.out.println (num_01);//获得字母编码范围“A(65)~Z(90)”,“a(97)~z(122)”,"0(48)~9(57)"
+        //小写字母比大写字母小32个编号
+        char ch_01 = '人';
+        int num_02 = ch_01;
+        System.out.println (num_02);
+    }
+}
