321a6de82cd1ba2d92a636495741897fdc6219bb
chaz <81009972+as916735173@users.noreply.github.com>
2021-08-12 15:11:52 +0800

    Initial commit

---
+++ src/java_01/test_08.java
@@ -0,0 +1,20 @@
+package java_01;
+
+public
+class test_08 {
+    public static void main(String[] arg){
+        double max=Double.MAX_VALUE;
+        double min=Double.MIN_VALUE;
+        double z=21.99999999999999;
+        System.out.println (z);
+        System.out.println (max);
+        System.out.println (min);
+        float max_02=Float.MAX_VALUE;
+        float min_02=Float.MIN_VALUE;
+        float x=(float) 10.23;
+        float y=10.36F;
+        System.out.println (x*y);
+        System.out.println (max_02);
+        System.out.println (min_02);
+    }
+}
