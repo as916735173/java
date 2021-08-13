228ff26818bc3f74bf855a9a2c59967096bff72e
chaz <81009972+as916735173@users.noreply.github.com>
2021-08-13 09:15:36 +0800

    Initial commit

--- src/java_01/test_10.java
+++ src/java_01/test_10.java
@@ -4,7 +4,8 @@ public
 class test_10 {
     public static void main(String[] arg){
         boolean flag = true ;
-        if (flag) {//判断flag内容，是true就执行
+        //判断flag内容，是true就执行
+        if (flag) {
             System.out.println ("我很帅，我很年轻！");
         }
     }
