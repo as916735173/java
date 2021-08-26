015141ec3574a150b72c8e6f940e0eb74382a599
chaz <81009972+as916735173@users.noreply.github.com>
2021-08-26 15:51:01 +0800

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	out/production/java/java_01/test_11.class

--- src/classesObjects/Constructors.java
+++ src/classesObjects/Constructors.java
@@ -2,12 +2,18 @@ package classesObjects;
 /**
  * @author chaz
  * 构造方法与匿名对象
+ *   ·声明并实例化对象，这个时候实例化对象中的属性并没有任何的数据存在
+ *   ·需要通过一系列的setter方法为类中的属性设置内容
+ *   ·重复setter方法使用时，可以通过构造方法实现实例化对象中的属性初始化处理，在关键字new时使用
+ * Java程序里的构造方法定义：
+ *      1.构造方法名称必须与类名称保持一致
+ *      2、构造方法不允许设置任何的返回值类型---即：没有返回值
+ *      3、构造方法是在使用关键字new实例化对象的时候自动调用的
  */
 class PersonFr {
     private String name;
     private int age;
-    public
-    void tell () {
+    public void tell () {
         System.out.println ( "姓名：" + name + "、 年龄：" + age );
     }
     //设置姓名属性
@@ -18,11 +24,11 @@ class PersonFr {
     //设置年龄属性
     public
     void setAge ( int a ) {
-        //age=a;
+        age=a;
         //限制输入数字，不满足返回0
-        if (a >= 0) {
+        /*if (a >= 0) {
             age = a;
-        }
+        }*/
     }
     //获取姓名属性
     public
@@ -39,6 +45,7 @@ class PersonFr {
     class Constructors {
         public static
         void main ( String[] args ) {
+            //1、对象的初始化准备。2、
             //声明并实例化对象
             PersonFr per = new PersonFr ();
         /*//在类外部修改属性
@@ -47,6 +54,7 @@ class PersonFr {
             //使用方法
             per.setName ( "张三" );
             per.setAge ( 28 );
+            //2、对象的使用
             //方法调用
             per.tell ();
         }
